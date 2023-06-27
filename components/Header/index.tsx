511b82128052591d090e057542691ca2677454e8
HoangCongMin <0782708546hoangcongminh@gmail.com>
2023-06-27 09:53:05 +0700

    fixsubmenu

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -119,7 +119,7 @@ const Header = () => {
             </div>
             <nav
               id="navbarCollapse"
-              className={`bg-nav-mobile xl:bg-none navbar fixed top-[80px] md:top-[90px] lg:top-[150px] right-0 bottom-0 z-30 ease border-body-color/50 bg-white px-4 py-4 duration-300 dark:border-body-color/20 dark:bg-dark xl:visible xl:static xl:w-auto xl:border-none xl:!bg-transparent xl:p-0 xl:opacity-100 ${
+              className={`overflow-y-auto xl:overflow-y-innital bg-nav-mobile xl:bg-none navbar fixed top-[80px] md:top-[90px] lg:top-[150px] right-0 bottom-0 z-30 ease border-body-color/50 bg-white px-4 py-4 duration-300 dark:border-body-color/20 dark:bg-dark xl:visible xl:static xl:w-auto xl:border-none xl:!bg-transparent xl:p-0 xl:opacity-100 ${
                 navbarOpen
                   ? "visibility left-[0] opacity-100"
                   : "invisible left-[-100%] opacity-0"
@@ -132,6 +132,7 @@ const Header = () => {
                       <>
                         {menuItem?.children.length > 0 ? (
                           <span
+                          onClick={() => handleSubmenu(index)}
                             className={`flex items-center justify-between py-3 text-base xl:text-[12px] 2xl:text-base text-white xl:text-dark group-hover:text-[green] cursor-pointer uppercase font-bold dark:text-white lg:mr-0 lg:inline-flex lg:py-6 lg:px-0`}
                           >
                             {menuItem.title}
@@ -139,6 +140,7 @@ const Header = () => {
                           </span>
                         ) : (
                           <Link
+                         
                             href={`/${handleItemNavBar(
                               menuItem.multiple_language
                             )}`}
@@ -150,17 +152,19 @@ const Header = () => {
 
                         {menuItem?.children.length > 0 && (
                           <div
-                            className={`submenu relative top-full left-0 rounded-md bg-white transition-[top] duration-300 group-hover:opacity-100 dark:bg-dark lg:invisible lg:absolute lg:top-[110%] lg:block lg:w-[250px] lg:p-4 lg:opacity-0 lg:shadow-lg lg:group-hover:visible lg:group-hover:top-full ${
+                            className={`submenu relative top-full left-0 rounded-md pl-[20px] bg-none xl:bg-white transition-[top] duration-300 group-hover:opacity-100 dark:bg-dark xl:invisible xl:absolute xl:top-[110%] xl:block xl:w-[250px] xl:p-4 xl:opacity-0 xl:shadow-lg xl:group-hover:visible xl:group-hover:top-full ${
                               openIndex === index ? "block" : "hidden"
                             }`}
                           >
                             {menuItem?.children.map((submenuItem) => (
                               <Link
+                              onClick={navbarToggleHandler}
+
                                 href={`/${handleItemNavBar(
                                   menuItem.multiple_language
                                 )}/${submenuItem.first_id === -1 ?String(submenuItem.id): String(submenuItem.first_id)}`}
                                 key={submenuItem.id}
-                                className="block rounded py-2.5 text-sm text-dark hover:text-[green] font-semibold dark:text-white lg:px-3"
+                                className="block rounded py-2.5 text-sm text-white xl:text-dark hover:text-[green] font-semibold dark:text-white lg:px-3"
                               >
                                 {submenuItem.title}
                               </Link>
